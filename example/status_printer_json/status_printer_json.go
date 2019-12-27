e8d17934e250e3df4264c3f9debf23bf448244b8
perillaroc <perillaroc@gmail.com>
2019-12-27 20:48:42 +0800

    :white_check_mark: use infinite loop.

--- example/status_printer_json/status_printer_json.go
+++ example/status_printer_json/status_printer_json.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nwpc-oper/ecflow-client-go"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -15,32 +16,37 @@ func main() {
 	outputFile := flag.String("output", "", "output file, default is os.Stdout")
 	flag.Parse()
 
-	client := ecflow_client.CreateEcflowClient(*ecflowHost, *ecflowPort)
-	defer client.Close()
+	for {
+		client := ecflow_client.CreateEcflowClient(*ecflowHost, *ecflowPort)
 
-	var err error
-	target := os.Stdout
-	if *outputFile != "" {
-		target, err = os.Create(*outputFile)
-		if err != nil {
-			panic(err)
+		var err error
+		target := os.Stdout
+		if *outputFile != "" {
+			target, err = os.Create(*outputFile)
+			if err != nil {
+				client.Close()
+				panic(err)
+			}
 		}
+
+		ret := client.Sync()
+		if ret != 0 {
+			log.Fatal("sync has error")
+		}
+
+		recordsJson := client.StatusRecordsJson()
+		var records []ecflow_client.StatusRecord
+		err = json.Unmarshal([]byte(recordsJson), &records)
+		if err != nil {
+			fmt.Printf("This is some error: %v\n", err)
+			return
+		}
+		//for _, record := range records {
+		//	fmt.Fprintf(target, "%s %s\n", record.Path, record.Status)
+		//}
+		fmt.Fprintf(target, "%d nodes\n", len(records))
+		client.Close()
+		time.Sleep(10 * time.Second)
 	}
 
-	ret := client.Sync()
-	if ret != 0 {
-		log.Fatal("sync has error")
-	}
-
-	recordsJson := client.StatusRecordsJson()
-	var records []ecflow_client.StatusRecord
-	err = json.Unmarshal([]byte(recordsJson), &records)
-	if err != nil {
-		fmt.Printf("This is some error: %v\n", err)
-		return
-	}
-	for _, record := range records {
-		fmt.Fprintf(target, "%s %s\n", record.Path, record.Status)
-	}
-	fmt.Fprintf(target, "%d nodes\n", len(records))
 }
